021041ef24e6ed639095d117e3f458b453396c9c
kamaneicha <kamaneicha@live.com>
2022-05-21 12:03:48 +0200

    Add project files.

---
+++ ConsoleLab/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoleLab
+{
+    class Program
+    {
+        //Наследование классов - дочерний класс наследует все поля и методы родительского класса
+        //Ассоциация классов - один класс содержится внутри другого в виде поля
+            //Агрегация и композиция
+
+        /*
+    class Car
+    {
+        public Engine eng { get; set; }
+
+        public Car()
+        {
+            eng = new Engine();
+        }
+    }
+    
+         */
+        static void Main(string[] args)
+        {
+            Human human = new Human(new CellPhone());
+            //human.age = -5;
+            human.SetAge(-5);
+        }
+    }
+}
