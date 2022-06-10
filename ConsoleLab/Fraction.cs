736941e18d8a57c7702f38b0670cb87fd5f14df2
kamaneicha <kamaneicha@live.com>
2022-06-10 11:15:52 +0200

    10/06/2022

--- ConsoleLab/Fraction.cs
+++ ConsoleLab/Fraction.cs
@@ -16,6 +16,12 @@ namespace ConsoleLab
 
         public T1 foo()
         {
+            Type type = typeof(T1);
+            if(type.IsPrimitive && type.Name != "Boolean")
+            {
+                double doll2 = Convert.ToDouble(doll);
+                doll2 *= 2;
+            }
             return doll;
         }
     }
@@ -34,6 +40,21 @@ namespace ConsoleLab
         public Fraction Mul(Fraction f)
         {
             return new Fraction(this.n * f.n, this.d * f.d);
+        }
+
+        public Fraction Add(int a)
+        {
+            return new Fraction(n + a * d, d);
+        }
+
+        public static Fraction operator +(Fraction f, int a)
+        {
+            return f.Add(a);
+        }
+
+        public static Fraction operator +(int a, Fraction f)
+        {
+            return f.Add(a);
         }
 
         public static Fraction operator *(Fraction f, Fraction c)
--- ConsoleLab/Program.cs
+++ ConsoleLab/Program.cs
@@ -10,69 +10,28 @@ namespace ConsoleLab
     {
         static void Main(string[] args)
         {
+            int i = 5;
+            double d = 7.5;
+            char c = 'a';
+            string s = "hello";
+            Fraction f = new Fraction(1, 2);
 
-            //Action == delegate void
-            //Predicate == delegate bool
-            //Func == delegate *
+            foo(i);
+            foo(d);
+            foo(c);
+            foo(s);
+            foo(f);
 
-            List<int> vs = new List<int>();
-
-            vs.Sort();
-
-            string s = null;
-            Console.WriteLine(s ?? "null");
-            
-            SayHello hello = SayHelloWorld;
-            hello -= SayHelloWorld;
-
-            Action hello2 = SayHelloWorld;
-            Predicate<int> p = isOdd;
-            Func<int, bool> test = isOdd;
-
-            Func<int, char, double> test2 = Test;
-
-
-            Class<double> f = new Class<double>(4);
-            Type type = f.foo().GetType();
-            Console.WriteLine(type);
-
-            /*
-             * Action Func Predicate Task
-             * 
-             поля:
-                value - число
-             методы:
-                конструктор
-                метод, который умножает число на 2 и возвращает его
-             */
+            //Teacher, Student, Manager, Admin, Trial, Lead
+            //List<Teacher>, List<Student>....
         }
 
-        static double Test(int a, char b)
+        //DRY - Don't Repeat Yourself
+        public static void foo<T>(T num)
         {
-            return 0.0;
+            dynamic dnum = num;
+            dynamic two = 2;
+            Console.WriteLine(two + dnum);
         }
-
-        delegate void SayHello();
-        //delegate void Action();
-
-        delegate bool NumberProperty(int value);
-        //delegate bool Predicate();
-
-        static bool IsEven(int a)
-        {
-            return a % 2 == 0;
-        }
-
-        static bool isOdd(int a) => a % 2 != 0;
-        static void SayHelloWorld()
-        {
-            Console.WriteLine("Hello World");
-        }
-
-        static void PrintImHere()
-        {
-            Console.WriteLine("I'm here!");
-        }
-
     }
 }
